22b935973b1b8165b19f7f9aafe6ee4cc0e8e73a
BeatrizMariaPessoa <pessoa.beatriz@escolar.ifrn.edu.br>
2024-10-21 10:25:35 -0300

    pag28 cap9

--- refeito/loja/views/UsuarioView.py
+++ refeito/loja/views/UsuarioView.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
+from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
+
+
 def list_usuario_view(request, id=None):
     # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
@@ -7,3 +10,37 @@ def list_usuario_view(request, id=None):
     'usuarios': usuarios
     }
     return render(request, template_name='usuario/usuario.html', context=context, status=200)
+def list_usuario_view(request, id=None):
+    # carrega somente usuarios, não inclui os admin
+    usuarios = Usuario.objects.filter(perfil=2)
+    context = {
+    'usuarios': usuarios
+    }
+    return render(request, template_name='usuario/usuario.html', context=context, status=200)
+
+# adicione o método de edição
+def edit_usuario_view(request):
+    usuario = get_object_or_404(Usuario, user=request.user)
+    # Adicione as linhas a seguir
+    # Perceba que os forms foram transferidos para dentro do if e do else
+    emailUnused = True
+    if request.method == 'POST':
+        usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
+        userForm = UserForm(request.POST, instance=request.user)
+        # Verifica se o e-mail que o usuário está tentando utilizar
+        # em seu perfil já existe em outro perfil
+        verifyEmail = Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+        if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+            usuarioForm.save()
+            userForm.save()
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
+    # Até aqui antes do context
+    context = {
+        'usuarioForm': usuarioForm,
+        'userForm': userForm
+    }
+    return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
+
