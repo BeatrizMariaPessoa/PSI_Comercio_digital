447a639978fe54afed9634d293afb8ae91554575
BeatrizMariaPessoa <pessoa.beatriz@escolar.ifrn.edu.br>
2025-01-20 10:29:44 -0300

    terminei mas ta dando erro

--- refeito/loja/views/CarrinhoView.py
+++ refeito/loja/views/CarrinhoView.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from loja.models import Produto, Carrinho, CarrinhoItem
+from loja.models import Produto, Carrinho, CarrinhoItem, Usuario
 from datetime import datetime
+
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
 # Função para adicionar um item ao carrinho
 def create_carrinhoitem_view(request, produto_id=None):
     print ('create_carrinhoitem_view')
@@ -12,18 +16,18 @@ def create_carrinhoitem_view(request, produto_id=None):
     print ('carrinho: ' + str(carrinho_id))
     carrinho = None
     if carrinho_id:
-    # Se o carrinho já estiver na sessão, tentamos obter o carrinho
-    carrinho = Carrinho.objects.filter(id=carrinho_id).first()
-    print (carrinho)
-    print ('carrinho1: ' + str(carrinho.id))
-    hoje = datetime.today().date()
-    # Caso queira define uma expiração do carrinho
-    if carrinho.criado_em.date() != hoje:
-        # Se o carrinho não for de hoje, cria um novo carrinho
-        carrinho = Carrinho.objects.create()
-        # Armazena o ID do carrinho na sessão
-        request.session['carrinho_id'] = carrinho.id
-        print ('novo carrinho: ' + str(carrinho.id))
+        # Se o carrinho já estiver na sessão, tentamos obter o carrinho
+        carrinho = Carrinho.objects.filter(id=carrinho_id).first()
+        print (carrinho)
+        print ('carrinho1: ' + str(carrinho.id))
+        hoje = datetime.today().date()
+        # Caso queira define uma expiração do carrinho
+        if carrinho.criado_em.date() != hoje:
+            # Se o carrinho não for de hoje, cria um novo carrinho
+            carrinho = Carrinho.objects.create()
+            # Armazena o ID do carrinho na sessão
+            request.session['carrinho_id'] = carrinho.id
+            print ('novo carrinho: ' + str(carrinho.id))
     else:
         # Se o carrinho não existir na sessão, cria um novo carrinho
         carrinho = Carrinho.objects.create()
@@ -31,13 +35,13 @@ def create_carrinhoitem_view(request, produto_id=None):
         request.session['carrinho_id'] = carrinho.id
         print ('carrinho2: ' + str(carrinho.id))
     # Verifica se o produto já existe no carrinho do usuário
-    carrinho_item = CarrinhoItem.objects.filter(carrinho=carrinho,produto=produto).first()
+    carrinho_item = CarrinhoItem.objects.filter(carrinho=carrinho, produto=produto).first()
     if carrinho_item:
-        # Se o produto já estiver no carrinho, apenas aumenta a quantidade
+    # Se o produto já estiver no carrinho, apenas aumenta a quantidade
         carrinho_item.quantidade += 1
         print ('item de carrinho: Acrescentou 1 item do produto ' + str(carrinho_item.id))
     else:
-            # Se o produto não estiver no carrinho, cria um novo item no carrinho
+        # Se o produto não estiver no carrinho, cria um novo item no carrinho
         carrinho_item = CarrinhoItem.objects.create(
             carrinho=carrinho,
             produto=produto,
@@ -52,6 +56,7 @@ def create_carrinhoitem_view(request, produto_id=None):
 def list_carrinho_view(request):
     print ('list_carrinho_view')
     carrinho = None
+    carrinho_item = None
     # Tenta pegar o carrinho da sessão ou cria um novo carrinho
     carrinho_id = request.session.get('carrinho_id')
     if carrinho_id:
@@ -59,7 +64,6 @@ def list_carrinho_view(request):
         # Obtém o carrinho do usuário
         carrinho = Carrinho.objects.filter(id=carrinho_id).first()
         print ('Data do carrinho' + str(carrinho.criado_em) )
-        carrinho_item = None
         # Verifica se o produto já existe no carrinho do usuário
         carrinho_item = CarrinhoItem.objects.filter(carrinho_id=carrinho_id)
         if carrinho_item:
@@ -69,3 +73,35 @@ def list_carrinho_view(request):
         'itens': carrinho_item
     }
     return render(request, 'carrinho/carrinho-listar.html', context=context)
+
+@login_required
+def confirmar_carrinho_view(request):
+    print ('confirmar_carrinho_view')
+    carrinho = None
+    # Tenta pegar o carrinho da sessão ou cria um novo carrinho
+    carrinho_id = request.session.get('carrinho_id')
+    if carrinho_id:
+        print ('carrinho: ' + str(carrinho_id))
+        # Obtém o carrinho do usuário
+        carrinho = Carrinho.objects.filter(id=carrinho_id).first()
+        # Obtém o usuário
+        usuario = get_object_or_404(Usuario, user=request.user)
+        print ('Usuario: ' + str(usuario))
+        if usuario:
+            carrinho.user_id = usuario.id
+            carrinho.situacao = 1
+            carrinho.confirmado_em = timezone.make_aware(datetime.today())
+            carrinho.save()
+            print ('carrinho salvo')
+    context = {
+        'carrinho': carrinho
+    }
+    return render(request, 'carrinho/carrinho-confirmado.html', context=context)
+
+def remover_item_view(request, item_id):
+    item = get_object_or_404(CarrinhoItem, id=item_id)
+    # Verifica se o item pertence ao carrinho do usuário (opcional)
+    carrinho_id = request.session.get('carrinho_id')
+    if carrinho_id == item.carrinho.id:
+        item.delete()
+    return redirect('/carrinho')
