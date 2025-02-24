b59dae5b271c73549f3caf66be3f2a4a27debaa0
BEATRIZ MARIA DANTAS MACEDO PESSOA <20221011110015@ifrn.local>
2025-02-24 11:19:03 -0300

    mudei os hosts

--- refeito/lojaAdmin/settings/production.py
+++ refeito/lojaAdmin/settings/production.py
@@ -1,7 +1,7 @@
 from .settings import *
 DEBUG = False
 #Crie a secret key para seu ambiente de produção
-SECRET_KEY = 'ixb6fha#ts=&b4t2u%p1_62-!8dw2j==j)d^3-j$!z(@*m+-h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default':{
