450707835a7e1f13748f84c03d4def8728f528dd
BeatrizMariaPessoa <pessoa.beatriz@escolar.ifrn.edu.br>
2025-02-24 09:36:42 -0300

    teste

--- refeito/loja/urls/AuthUrls.py
+++ refeito/loja/urls/AuthUrls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from loja.views.AuthView import login_view, register_view, logout_view
+from loja.views.AuthView import login_view, register_view, logout_view, recovery_view
 
 urlpatterns = [
     path("login", login_view, name='login'),
     path("register", register_view, name='register'),
     path("logout", logout_view, name='logout'),
+    path("recovery", recovery_view, name='recovery')
 ]
--- refeito/lojaAdmin/settings/settings.py
+++ refeito/lojaAdmin/settings/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['psi-comercio-digital.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
